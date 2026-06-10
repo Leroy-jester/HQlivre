648fb83476669586d99db067c1d568e24080e8c2
Leroy-jester <josefmelcher@gmail.com>
2026-06-10 18:16:44 -0400

    adicionando o delete

--- components/Crud.tsx
+++ components/Crud.tsx
@@ -1,275 +1,275 @@
 import * as SQLite from 'expo-sqlite';
 
-let db: SQLite.SQLiteDatabase;
+    let db: SQLite.SQLiteDatabase;
 
-export async function getDB() {
-    if (!db) {
-        db = await SQLite.openDatabaseAsync('NossoBanco');
+    export async function getDB() {
+        if (!db) {
+            db = await SQLite.openDatabaseAsync('NossoBanco');
+        }
+
+        return db;
     }
 
-    return db;
-}
+    //crud para usar online, para quando cair a internet ir salvando no celular e daí enviar para o site quando voltar a conecção, usando o expo-sqlite
 
-//crud para usar online, para quando cair a internet ir salvando no celular e daí enviar para o site quando voltar a conecção, usando o expo-sqlite
-
-type Manga = {
-    id: string;
-    image_uri: string;
-    nome: string;
-    autor: string;
-    chapters: number;
-    status: string;
-    note: number;
-    description: string;
-    generos: Gender[];
-}
-type Gender = {
-    id: string;
-    nome_genero: string;
-}
-type MangaCompleto = Manga & {
-    generos: Gender[];
-}
-
-/* O Crud precisa ser capaz de fazer 
-PUSH; 
-GET ALL; 
-GET by id,name,autor,gender; 
-PATCH, 
-DELETE*/
-
-const criaBD = async () => {
-  const bd = await SQLite.openDatabaseAsync('NossoBanco');
-
-  await bd.execAsync(`
-    PRAGMA journal_mode = WAL;
-    CREATE TABLE mangas (
-        id TEXT PRIMARY KEY,
-        image_uri TEXT,
-        nome TEXT NOT NULL,
-        autor TEXT NOT NULL,
-        chapters INTEGER,
-        status TEXT,
-        note REAL,
-        description TEXT,
-        sync_status TEXT DEFAULT 'pending',
-        created_at TEXT,
-        updated_at TEXT
-    );
-
-    CREATE TABLE genders (
-        id TEXT PRIMARY KEY,
-        nome_genero TEXT NOT NULL
-    );
-
-    CREATE TABLE IF NOT EXISTS mangas_genders (
-        manga_id TEXT,
-        genero_id TEXT,
-        PRIMARY KEY(manga_id, genero_id),
-        FOREIGN KEY (manga_id) REFERENCES mangas(id) ON DELETE CASCADE,
-        FOREIGN KEY (genero_id) REFERENCES genders(id) ON DELETE CASCADE
-    );
-    `);
-  bd.closeAsync();
-}
-// função para não ficar repetindo código
-
-const montarManga = async (manga: Manga): Promise<MangaCompleto> => {
-    const bd = await getDB();
-
-    const generos = await bd.getAllAsync<Gender>(
-        `
-        SELECT g.*
-        FROM genders g
-        INNER JOIN mangas_genders mg
-            ON g.id = mg.genero_id
-        WHERE mg.manga_id = ?
-        `,
-        [manga.id]
-    );
-
-    return {
-        ...manga,
-        generos
-    };
-};
-
-//GET ALL, retorna todos os mangas postados e seus gêneros 
-export const pegarMangas = async () => {
-    const bd = await getDB();
-
-    const mangas = await bd.getAllAsync<Manga>(
-        'SELECT * FROM mangas'
-    );
-
-    return Promise.all(
-        mangas.map(montarManga)
-    );
-};
-//GET by NOME, retorna todos os mangas com o nome parecido com oque foi escrito
-export const pegarMangasPorNome = async (nome: string) => {
-    const bd = await getDB();
-
-    const mangas = await bd.getAllAsync<Manga>(
-        `
-        SELECT *
-        FROM mangas
-        WHERE nome LIKE ?
-        `,
-        [`%${nome}%`]
-    );
-
-    return Promise.all(
-        mangas.map(montarManga)
-    );
-};
-//GET by GENDER, retorna todos os mangas baseados no gênero
-export const pegarMangasPorGenero = async (generoId: string) => {
-    const bd = await getDB();
-
-    const mangas = await bd.getAllAsync<Manga>(
-        `
-        SELECT m.*
-        FROM mangas m
-        INNER JOIN mangas_genders mg
-            ON m.id = mg.manga_id
-        WHERE mg.genero_id = ?
-        `,
-        [generoId]
-    );
-
-    return Promise.all(
-        mangas.map(montarManga)
-    );
-};
-//GET by autor, retorna todas as obras que aquele escritor já fez obs: quando for postar um mangá dar opção baseado nos mangakás já colocado lá
-export const pegarMangasPorAutor = async (autor: string) => {
-    const bd = await getDB();
-
-    const mangas = await bd.getAllAsync<Manga>(
-        `
-        SELECT *
-        FROM mangas
-        WHERE autor LIKE ?
-        `,
-        [`%${autor}%`]
-    );
-
-    return Promise.all(
-        mangas.map(montarManga)
-    );
-};
-
-/* Vai retornar algo parecido com isso
-
-{
-  "id": "123",
-  "nome": "Re:Zero",
-  "autor": "Tappei Nagatsuki",
-  "chapters": 60,
-  "note": 4,
-  "image_uri": "...",
-  "generos": [
-    {
-      "id": "16",
-      "nome_genero": "Fantasia"
-    },
-    {
-      "id": "24",
-      "nome_genero": "Isekai"
+    type Manga = {
+        id: string;
+        image_uri: string;
+        nome: string;
+        autor: string;
+        chapters: number;
+        status: string;
+        note: number;
+        description: string;
+        generos: Gender[];
     }
-  ]
-}
-*/
+    type Gender = {
+        id: string;
+        nome_genero: string;
+    }
+    type MangaCompleto = Manga & {
+        generos: Gender[];
+    }
 
-//Post
+    /* O Crud precisa ser capaz de fazer 
+    PUSH; 
+    GET ALL; 
+    GET by id,name,autor,gender; 
+    PATCH, 
+    DELETE*/
 
-export const enviarManga = async (
-    manga: Manga,
-    generosIds: string[]
-) => {
-    const bd = await getDB();
+    const criaBD = async () => {
+    const bd = await SQLite.openDatabaseAsync('NossoBanco');
 
-    await bd.withTransactionAsync(async () => {
-
-        await bd.runAsync(
-            `
-            INSERT INTO mangas (
-                id, 
-                image_uri, 
-                nome, 
-                autor, 
-                chapters, 
-                status, 
-                note, 
-                description, 
-                created_at, 
-                updated_at
-            )
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            `,
-            [
-                manga.id,
-                manga.image_uri,
-                manga.nome,
-                manga.autor,
-                manga.chapters,
-                manga.status,
-                manga.note,
-                manga.description,
-                new Date().toISOString(),
-                new Date().toISOString()
-            ]
+    await bd.execAsync(`
+        PRAGMA journal_mode = WAL;
+        CREATE TABLE mangas (
+            id TEXT PRIMARY KEY,
+            image_uri TEXT,
+            nome TEXT NOT NULL,
+            autor TEXT NOT NULL,
+            chapters INTEGER,
+            status TEXT,
+            note REAL,
+            description TEXT,
+            sync_status TEXT DEFAULT 'pending',
+            created_at TEXT,
+            updated_at TEXT
         );
 
-        for (const generoId of generosIds) {
+        CREATE TABLE genders (
+            id TEXT PRIMARY KEY,
+            nome_genero TEXT NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS mangas_genders (
+            manga_id TEXT,
+            genero_id TEXT,
+            PRIMARY KEY(manga_id, genero_id),
+            FOREIGN KEY (manga_id) REFERENCES mangas(id) ON DELETE CASCADE,
+            FOREIGN KEY (genero_id) REFERENCES genders(id) ON DELETE CASCADE
+        );
+        `);
+    bd.closeAsync();
+    }
+    // função para não ficar repetindo código
+
+    const montarManga = async (manga: Manga): Promise<MangaCompleto> => {
+        const bd = await getDB();
+
+        const generos = await bd.getAllAsync<Gender>(
+            `
+            SELECT g.*
+            FROM genders g
+            INNER JOIN mangas_genders mg
+                ON g.id = mg.genero_id
+            WHERE mg.manga_id = ?
+            `,
+            [manga.id]
+        );
+
+        return {
+            ...manga,
+            generos
+        };
+    };
+
+    //GET ALL, retorna todos os mangas postados e seus gêneros 
+    export const pegarMangas = async () => {
+        const bd = await getDB();
+
+        const mangas = await bd.getAllAsync<Manga>(
+            'SELECT * FROM mangas'
+        );
+
+        return Promise.all(
+            mangas.map(montarManga)
+        );
+    };
+    //GET by NOME, retorna todos os mangas com o nome parecido com oque foi escrito
+    export const pegarMangasPorNome = async (nome: string) => {
+        const bd = await getDB();
+
+        const mangas = await bd.getAllAsync<Manga>(
+            `
+            SELECT *
+            FROM mangas
+            WHERE nome LIKE ?
+            `,
+            [`%${nome}%`]
+        );
+
+        return Promise.all(
+            mangas.map(montarManga)
+        );
+    };
+    //GET by GENDER, retorna todos os mangas baseados no gênero
+    export const pegarMangasPorGenero = async (generoId: string) => {
+        const bd = await getDB();
+
+        const mangas = await bd.getAllAsync<Manga>(
+            `
+            SELECT m.*
+            FROM mangas m
+            INNER JOIN mangas_genders mg
+                ON m.id = mg.manga_id
+            WHERE mg.genero_id = ?
+            `,
+            [generoId]
+        );
+
+        return Promise.all(
+            mangas.map(montarManga)
+        );
+    };
+    //GET by autor, retorna todas as obras que aquele escritor já fez obs: quando for postar um mangá dar opção baseado nos mangakás já colocado lá
+    export const pegarMangasPorAutor = async (autor: string) => {
+        const bd = await getDB();
+
+        const mangas = await bd.getAllAsync<Manga>(
+            `
+            SELECT *
+            FROM mangas
+            WHERE autor LIKE ?
+            `,
+            [`%${autor}%`]
+        );
+
+        return Promise.all(
+            mangas.map(montarManga)
+        );
+    };
+
+    /* Vai retornar algo parecido com isso
+
+    {
+    "id": "123",
+    "nome": "Re:Zero",
+    "autor": "Tappei Nagatsuki",
+    "chapters": 60,
+    "note": 4,
+    "image_uri": "...",
+    "generos": [
+        {
+        "id": "16",
+        "nome_genero": "Fantasia"
+        },
+        {
+        "id": "24",
+        "nome_genero": "Isekai"
+        }
+    ]
+    }
+    */
+
+    //Post
+
+    export const enviarManga = async (
+        manga: Manga,
+        generosIds: string[]
+    ) => {
+        const bd = await getDB();
+
+        await bd.withTransactionAsync(async () => {
+
             await bd.runAsync(
                 `
-                INSERT INTO mangas_genders
-                (manga_id, genero_id)
-                VALUES (?, ?)
+                INSERT INTO mangas (
+                    id, 
+                    image_uri, 
+                    nome, 
+                    autor, 
+                    chapters, 
+                    status, 
+                    note, 
+                    description, 
+                    created_at, 
+                    updated_at
+                )
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
                 `,
-                [manga.id, generoId]
+                [
+                    manga.id,
+                    manga.image_uri,
+                    manga.nome,
+                    manga.autor,
+                    manga.chapters,
+                    manga.status,
+                    manga.note,
+                    manga.description,
+                    new Date().toISOString(),
+                    new Date().toISOString()
+                ]
             );
-        }
-    });
-};
 
-//PACTH para atualizar os mangas e seus gêneros
+            for (const generoId of generosIds) {
+                await bd.runAsync(
+                    `
+                    INSERT INTO mangas_genders
+                    (manga_id, genero_id)
+                    VALUES (?, ?)
+                    `,
+                    [manga.id, generoId]
+                );
+            }
+        });
+    };
 
-export const atualizarManga = async (
-    id: string,
-    dados: Partial<Manga>
-) => {
-    const bd = await getDB();
+    //PACTH para atualizar os mangas e seus gêneros
 
-    const campos: string[] = [];
-    const valores: any[] = [];
+    export const atualizarManga = async (
+            id: string,
+            dados: Partial<Manga>
+        ) => {
+            const bd = await getDB();
 
-    Object.entries(dados).forEach(([chave, valor]) => {
-        campos.push(`${chave} = ?`);
-        valores.push(valor);
-    });
+            const campos: string[] = [];
+            const valores: any[] = [];
 
-    campos.push('updated_at = ?');
-    valores.push(new Date().toISOString());
+            Object.entries(dados).forEach(([chave, valor]) => {
+                campos.push(`${chave} = ?`);
+                valores.push(valor);
+            });
 
-    valores.push(id);
+            campos.push('updated_at = ?');
+            valores.push(new Date().toISOString());
 
-    await bd.runAsync(
-        `
-        UPDATE mangas
-        SET ${campos.join(', ')}
-        WHERE id = ?
-        `,
-        valores
-    );
-};
+            valores.push(id);
 
-//DELETE para apagar os outros dados do banco
+            await bd.runAsync(
+                `UPDATE mangas
+                SET ${campos.join(', ')}
+                WHERE id = ?`,
+            valores
+        );
+    };
 
-export const deletarManga = async (manga:Manga) => {
-    const bd = await getDB();
+    //DELETE para apagar os outros dados do banco
 
-    await bd.runAsync('DELETE * FROM mangas WHERE ?', manga.id)
-}
+    export const deletarManga = async (
+        id: string
+    ) => {
+        const bd = await getDB();
+
+        await bd.runAsync('DELETE * FROM mangas WHERE id = ?', id)
+    }
